e037f3a03d88b396977953671960617f1614c575
AmyKumar <info2amy@yahoo.com>
2021-01-28 17:14:38 -0600

    created application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,5 +11,18 @@ class ApplicationController < ActionController::API
     HashWithIndifferentAccess.new decoded
   end
 
+  def authorize_request
+    header = request.headers['Authorization']
+    header = header.split(' ').last if header
+    begin
+      @decoded = decode(header)
+      @current_user = User.find(@decoded[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
+      render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+
 
 end
