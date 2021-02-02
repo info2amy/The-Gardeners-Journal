e7d13a4c7bd499a81b155fb93ff8fe99b0782bcb
AmyKumar <info2amy@yahoo.com>
2021-02-02 16:21:18 -0600

    beginning CSS on main Plant screen

--- client/src/screens/Plants.jsx
+++ client/src/screens/Plants.jsx
@@ -8,35 +8,43 @@ export default function Plants(props) {
     <div>
       <h3>Plants, Bushes, Trees</h3>
       <h4>Click on a plant for detailed information.</h4>
-
       <Link to="/plants/new">
         <button>Add a new plant, bush or tree</button>
-      </Link> <br/> <br/>
+      </Link>
 
-      {plants.map((plant) => (
-        <React.Fragment key={plant.id}>
-          <Link to={`/plants/${plant.id}`}>
-            <img className="allImages" src={plant.image_url} alt="plantimage"/>
-            <p>{plant.name}</p>
-          </Link>
-            <p>{plant.name} blooms in {plant.bloom_time}</p>
-          {currentUser?.id  && (
-            <>
-              {/* <Link to={`/plants/${plant.id}/edit`}>
+
+
+      
+      <br /> <br />
+      <section className="plant-container">
+        {plants.map((plant) => (
+          <React.Fragment key={plant.id}>
+            <Link to={`/plants/${plant.id}`}>
+              <img
+                className="all-images"
+                src={plant.image_url}
+                alt="plantimage"
+              />
+              <p>{plant.name}</p>
+            </Link>
+            {/* <p>{plant.name} blooms in {plant.bloom_time}</p> */}
+            {currentUser?.id && (
+              <>
+                {/* <Link to={`/plants/${plant.id}/edit`}>
                 <button>Edit this information</button>
               </Link> */}
-              <br/>
-            </> 
-          )}
-          <br/> <br/>
-        </React.Fragment>
-      ))}
+                <br />
+              </>
+            )}
+            <br /> <br />
+          </React.Fragment>
+        ))}
+      </section>
       <br />
-      <br/>
+      <br />
       {/* <Link to="/plants/new">
         <button>Add a new plant, bush or tree</button>
       </Link> */}
     </div>
   );
 }
-
